a7601ae811f03f01cce89353e1d30c34cf2a3352
Varshini-ceo <varshinis3@student.tce.edu>
2025-10-31 15:47:30 +0530

    Update doctorservice.java

--- doctorservice.java
+++ doctorservice.java
@@ -1,1 +1,28 @@
+package com.smartclinic.service;
+
+import com.smartclinic.model.Doctor;
+import com.smartclinic.repository.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DoctorService {
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    public List<Doctor> getAllDoctors() {
+        return doctorRepository.findAll();
+    }
+
+    public Doctor addDoctor(Doctor doctor) {
+        return doctorRepository.save(doctor);
+    }
+
+    public Doctor getDoctorById(Long id) {
+        return doctorRepository.findById(id).orElse(null);
+    }
+}
 
