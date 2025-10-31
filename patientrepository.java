cfa17494ba0082feba6627d824f902db9fd755eb
Varshini-ceo <varshinis3@student.tce.edu>
2025-10-31 15:49:31 +0530

    Update patientrepository.java

--- patientrepository.java
+++ patientrepository.java
@@ -1,1 +1,8 @@
 
+package com.smartclinic.repository;
+
+import com.smartclinic.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+}
