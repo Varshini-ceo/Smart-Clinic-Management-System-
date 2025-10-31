c5b6a323603003cb8e6477333b06784fa266dd62
Varshini-ceo <varshinis3@student.tce.edu>
2025-10-31 15:48:45 +0530

    Update prescriptioncontroller.java

--- prescriptioncontroller.java
+++ prescriptioncontroller.java
@@ -1,1 +1,27 @@
 
+package com.smartclinic.controller;
+
+import com.smartclinic.model.Prescription;
+import com.smartclinic.service.PrescriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/prescriptions")
+public class PrescriptionController {
+
+    @Autowired
+    private PrescriptionService prescriptionService;
+
+    @PostMapping
+    public Prescription addPrescription(@RequestBody Prescription prescription) {
+        return prescriptionService.savePrescription(prescription);
+    }
+
+    @GetMapping
+    public List<Prescription> getAllPrescriptions() {
+        return prescriptionService.getAllPrescriptions();
+    }
+}
