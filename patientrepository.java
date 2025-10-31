161c04aa4b7898d1a72214ac52e1392d56e8650e
Varshini-ceo <varshinis3@student.tce.edu>
2025-10-31 16:30:29 +0530

    Update patientrepository.java

--- patientrepository.java
+++ patientrepository.java
@@ -1,8 +1,11 @@
 
-package com.smartclinic.repository;
+package com.clinic.repository;
 
-import com.smartclinic.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.clinic.entity.Patient;
+import java.util.Optional;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Optional<Patient> findByEmail(String email);
+    Optional<Patient> findByPhone(String phone);
 }
